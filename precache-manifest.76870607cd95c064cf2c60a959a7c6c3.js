881eb4bf3c6da832f3cc7eb8e578083424e5b2d0
Seth Purr <eric.purrington@gmail.com>
2020-07-25 13:54:45 -0700

    Updates

--- precache-manifest.76870607cd95c064cf2c60a959a7c6c3.js
+++ precache-manifest.76870607cd95c064cf2c60a959a7c6c3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "84cd705ed5b358c1a44be81325b56277",
+    "revision": "54805a13d16ad4956f229a42739e4a58",
     "url": "/React-Portfolio/index.html"
   },
   {
-    "revision": "e765d14ccb5227c935cc",
+    "revision": "9a21133712ede62cd706",
     "url": "/React-Portfolio/static/css/main.1a51c6a0.chunk.css"
   },
   {
-    "revision": "e7da514a97bd7844779b",
-    "url": "/React-Portfolio/static/js/2.b26e3eff.chunk.js"
+    "revision": "5b8a32950740b684f6d8",
+    "url": "/React-Portfolio/static/js/2.c283ad19.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/React-Portfolio/static/js/2.b26e3eff.chunk.js.LICENSE.txt"
+    "url": "/React-Portfolio/static/js/2.c283ad19.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e765d14ccb5227c935cc",
-    "url": "/React-Portfolio/static/js/main.1d2326de.chunk.js"
+    "revision": "9a21133712ede62cd706",
+    "url": "/React-Portfolio/static/js/main.d8903e6b.chunk.js"
   },
   {
     "revision": "7c35ff808cd94c4ce20c",
