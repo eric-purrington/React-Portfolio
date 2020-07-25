da050eb4d53cd66a6ee47dbf6497320bac089fd1
Seth Purr <eric.purrington@gmail.com>
2020-07-25 11:28:56 -0700

    Updates

---
+++ precache-manifest.5cb329f8ec6c351b2602fb7d32cb7a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cd705ed5b358c1a44be81325b56277",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "e765d14ccb5227c935cc",
+    "url": "/React-Portfolio/static/css/main.1a51c6a0.chunk.css"
+  },
+  {
+    "revision": "e7da514a97bd7844779b",
+    "url": "/React-Portfolio/static/js/2.b26e3eff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.b26e3eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e765d14ccb5227c935cc",
+    "url": "/React-Portfolio/static/js/main.1d2326de.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  }
+]);
