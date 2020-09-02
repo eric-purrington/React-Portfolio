040630d6aa77cbe60f8a26a34ecf65497b830c56
Seth Purr <eric.purrington@gmail.com>
2020-09-02 11:17:20 -0600

    took out dnd img

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,10 +6,10 @@ import lilme from "../../images/lilme.png";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <img className="myLilImg" src={lilme} alt="little dnd character" />
+            {/* <img className="myLilImg" src={lilme} alt="little dnd character" /> */}
             <a className="navbar-brand" href="/">Eric Purrington</a>
 
-            <button className="navbar-toggler" type="button" data-toggle="collapse"     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
         
